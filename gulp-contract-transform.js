16b5cf3354440543175c2526928027f83fe4ecf6
Duarte Aragao <daragao@gmail.com>
2016-06-19 18:00:14 +0100

    deploying contracts, but need to cycle through them

--- gulp-contract-transform.js
+++ gulp-contract-transform.js
@@ -2,6 +2,7 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var graphlib = require('graphlib');
 var Graph = require("graphlib").Graph;
+var Web3 = require('web3');
 var solc = require('solc');
 var Pudding = require('ether-pudding');
 var PluginError = gutil.PluginError;
@@ -85,13 +86,48 @@ contractTransform.compileContracts = function() {
     return stream;
 };
 
-contractTransform.deployContracts = function() {
+var deployContract = function(web3,g,contract,account,gas) {
+    gutil.log('Deploying:',contract.contract_name);
+    var regex = /__([^_]*)_*/g;
+    var match;
+    while(match = regex.exec(contract.binary)) {
+        var address = g.node(match[1]).address.slice(2);
+        //gutil.log(match[0],address);
+        contract.binary = contract.binary
+        .replace(RegExp(match[0],'g'),address);
+    }
+
+    contract.setProvider(web3.currentProvider);
+    contract.defaults({ from: account, gas: gas });
+    var deployedContract = contract.new(contract,{});
+    //gutil.log(contract.address,account);
+    deployedContract.then(function(contractInstance) {
+        gutil.log('Contract deployed with address:',contractInstance.address);
+        contract.address = contractInstance.address;
+        return contractInstance;
+    }).catch(function(err) {
+        console.log("Error creating contract!");
+        console.log(err.stack);
+    });
+
+    return deployedContract;
+};
+
+contractTransform.deployContracts = function(providerURL,account,gas) {
+
+    var web3 = new Web3();
+    providerURL = providerURL || 'http://localhost:8545';
+    var provider = new web3.providers.HttpProvider(providerURL);
+    web3.setProvider(provider);
+    account = account || web3.eth.accounts[0];
+    gas = gas || 300000;
 
     var g = new Graph();
 
     var stream = through.obj(function(file, enc, cb) {
         if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+            this.emit('error',
+                      new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
             return cb();
         }
 
@@ -118,20 +154,25 @@ contractTransform.deployContracts = function() {
             gutil.log(gutil.colors.red('THROW AN ERROR HERE! THERE ARE CYCLES!'));
         } else {
             var orderedNodes = graphlib.alg.topsort(g).reverse();
-            orderedNodes.forEach(function(contractName) {
-                //gutil.log(contractName);
-                var contract = g.node(contractName);
-                var regex = /__([^_]*)_*/g;
-                var reLib;
-                var match;
-                while(match = regex.exec(contract.binary)) {
-                    var address = '<<<<<<<<HELLOADDRESS>>>>>>>>>>';
-                    reLib = RegExp(match[0],'g')
-                    contract.binary = contract.binary.replace(reLib,address);
-                    gutil.log(contract.binary);
-                }
-            });
+
+            //var contract = g.nodorderedNodes[0]);
+            deployContract(web3,g,g.node(orderedNodes[0]),account,gas)
+            .then(deployContract.bind(this,web3,g,g.node(orderedNodes[1]),account,gas))
+            .then(deployContract.bind(this,web3,g,g.node(orderedNodes[2]),account,gas));
+/*
+            var contract = g.node(orderedNodes[0]);
+            deployContract(web3,g,contract).then(function(contractInstance) {
+                var newFile = new gutil.File({
+                    cwd: "",
+                    base: "",
+                    path: contractInstance.contract_name+'.sol.js',
+                    contents: new Buffer(contractInstance)
+                });
+                //this.push(newFile)
+                cb(null,newFile);
+            }.bind(this));
             //gutil.log(orderedNodes);
+            */
         }
         cb();
     });
